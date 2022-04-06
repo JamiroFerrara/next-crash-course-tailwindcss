bdd467e2105d148fbe273be2caad39a4c7e2d4b6
Jamie <83338941+JamiroFerrara@users.noreply.github.com>
2022-04-06 16:57:08 +0200

    initial commit

---
+++ components/Button.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Button = () => {
+  return (
+    <a
+      href=""
+      className=" mx-2 mt-6 max-w-xs rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-400"
+    >
+      <h3 className="mt text-2xl font-bold">Deploy &rarr;</h3>
+      <p className="mt-4 text-xl">
+        Instantly deploy your Next.js site to a public URL with Vercel.
+      </p>
+    </a>
+  )
+}
+
+export default Button
